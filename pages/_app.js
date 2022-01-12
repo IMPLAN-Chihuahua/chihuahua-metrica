042727ad047eba452b686b2626eb64895495daad
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-01-12 14:07:40 -0700

    Added header with default icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import createEmotionCache from '../styles/createEmotionCache';
+import  Head  from 'next/head';
+import "/styles/globals.css";
+import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Manually generated
+import Header from '@components/Header';
+
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <title>Observatorio Urbano</title>
+        <meta name='viewport' content='initial-scale=1, width=device-width'/>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <Header />
+        <Component {...pageProps}/>
+      </ThemeProvider>
+    </CacheProvider>);
 }
 
-export default MyApp
+export default MyApp;

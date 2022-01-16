fc406e5da9980627bf1df11be2df088c6d5faf9d
Luis Cruz <luisccc99@gmail.com>
2022-01-16 14:55:06 -0700

    Finish footer design

--- components/Footer.js
+++ components/Footer.js
@@ -1,71 +1,105 @@
-import { Grid, Typography } from '@mui/material';
-import SocialNetworksList from './SocialNetworksList';
+import { Stack, Box, Typography, Divider } from '@mui/material'
+import Image from 'next/image';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import styles from './Footer.module.css';
+import { Facebook, Twitter, Instagram } from '@mui/icons-material';
 import Link from 'next/link';
 
 const Footer = () => {
-    const socialNetworks = [
-        {
-            name: 'Facebook',
-            icon: '/images/facebook.svg',
-            url: 'https://www.facebook.com/Implan-Chihuahua-108606468095094'
-        },
-        {
-            name: 'Twitter',
-            icon: '/images/twitter.svg',
-            url: 'https://twitter.com/IMPLANChih?s=08'
-        },
-        {
-            name: 'Instagram',
-            icon: '/images/instagram.svg',
-            url: 'https://www.instagram.com/implanchihuahua/'
-        }
-    ];
+    const theme = useTheme();
+    const isMidSize = useMediaQuery(theme.breakpoints.down('md'));
+    let size = isMidSize ? { display: 'none' } : { height: '50px' };
     return (
-        <footer>
-            <Grid container sx={{
-                backgroundColor: 'primary.main',
-                color: 'primary.onMain',
-                textAlign: 'center',
-                fontWeight: 'regular',
-                paddingTop: '20px',
-                marginTop: '20px'
-            }}>
-                <Grid item xs={12} md={4}>
-                    <Typography>
-                        IMPLAN
-                    </Typography>
-                    <Typography>
-                        Contact us
-                    </Typography>
-                    <Typography>
-                        We are on
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Link href='/politicas'>
-                        <a>
-                            <Typography>
-                                Politcas de uso
-                            </Typography>
-                        </a>
-                    </Link>
-                    <Typography>
-                        Lorem
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} md={4}>
-                    <Typography>
-                        Mantente actualizado
-                    </Typography>
-                    <SocialNetworksList socialNetworks={socialNetworks} />
-                </Grid>
-                <Grid item xs={12} md={12}>
-                    <Typography variant='caption'>
-                        Derechos reservados {new Date().getFullYear() } por IMPLAN 
-                    </Typography>
-                </Grid>
-            </Grid>
-        </footer>
+        <Box
+            component='footer'
+            className={styles.footerContainer}
+            paddingLeft={5}
+            paddingRight={5}
+            paddingTop={4}
+            paddingBottom={4}
+            backgroundColor='primary.main'
+            color='primary.onMain'
+            marginTop={5}
+        >
+            <Stack
+                direction={{ xs: 'column', md: 'row' }}
+                spacing={4}
+                divider={
+                    <Divider
+                        sx={{
+                            ...size,
+                            border: '1px dashed #E8E8E8',
+                            alignSelf: 'center'
+                        }}
+                        variant='medium'
+                        light
+                        flexItem />
+                }
+                justifyContent='space-around'
+                mb={2}
+            >
+                <Box component='div' flexGrow={1} textAlign='center'>
+                    <Image src='/images/implan-logo.webp' width={200} height={150} />
+                </Box>
+                <Box component='div' flexGrow={1}>
+                    <Typography variant='h5' component='h3'>Más Información</Typography>
+                    <ul className={styles.footerList}>
+                        <li>
+                            <Link href='/'>
+                                <a className={styles.underline}>
+                                    Preguntas Frequentes
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href='/'>
+                                <a className={styles.underline}>
+                                    Objetivos
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </Box>
+                <Box component='div' flexGrow={1}>
+                    <Typography variant='h5' component='h3'>Legal</Typography>
+                    <ul className={styles.footerList}>
+                        <Link href='/'>
+                            <a className={styles.underline}>
+                                Politicas de Privacidad
+                            </a>
+                        </Link>
+                    </ul>
+                </Box>
+                <Box component='div' flexGrow={1}>
+                    <Typography variant='h5' component='h3'>Siguenos</Typography>
+                    <Stack direction='row' spacing={4} justifyContent='center' mt={2}>
+                        <Link href='https://www.facebook.com/Implan-Chihuahua-108606468095094' passHref={true}>
+                            <a target='_blank' rel='noreferrer noopener'
+                                className={styles.underline}>
+                                <Facebook fontSize='large' />
+                            </a>
+                        </Link>
+                        <Link href='https://twitter.com/IMPLANChih?s=08' passHref={true}>
+                            <a target='_blank' rel='noreferrer noopener'
+                                className={styles.underline}>
+                                <Twitter fontSize='large' />
+                            </a>
+                        </Link>
+                        <Link href='https://www.instagram.com/implanchihuahua/' passHref={true}>
+                            <a target='_blank' rel='noreferrer noopener'
+                                className={styles.underline}>
+                                <Instagram fontSize='large' />
+                            </a>
+                        </Link>
+
+                    </Stack>
+                </Box>
+            </Stack>
+            <Typography variant='caption' align='center' display='block'>
+                &copy; {new Date().getFullYear()} Instituto de Planeación Integral del Municipio de Chihuahua
+            </Typography>
+        </Box>
     );
 };
 

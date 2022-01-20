b3ac3d2f7f750779f66e31dce23102a9d84e8b9b
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-01-19 22:36:36 -0700

    Test with SWR to fetch data from one singular indicador

---
+++ pages/indicador/[idIndicador].js
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+import useSWR from 'swr';
+
+export default function FichaTecnica(props) {
+    const data = props.data;
+    const fichaIndicador = data.map();
+}
+
+export async

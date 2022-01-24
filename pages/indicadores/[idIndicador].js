9c512a56b0aac47e3a7575f7de78bb386db869f2
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-01-24 10:01:32 -0700

    Fixed merging conflicts

--- pages/indicadores/[idIndicador].js
+++ pages/indicadores/[idIndicador].js
@@ -145,126 +145,50 @@ export default function FichaTecnica(props) {
           </Grid>
         </Grid>
         {/*=============================== Cuerpo ======================================*/}
-        <Grid container xs={12} md={12}>
-          <Grid xs={12} md={6} sx={{ bgcolor: "" }}>
-            <Grid container spacing={3} sx={{ mt: "1%" }}>
-              <Information
-                header={data.ultimoValorDisponible}
-                title="Último valor disponible"
-                body="TBD databse"
-              />
-              <Information
-                header={data.tendenciaActual}
-                title="Tendencia actual"
-                body={data.tendenciaDeseada}
-              />
+        <Grid container item xs={12} md={12} >
+                <Grid item xs={12} md={6} sx={{bgcolor: ''}}>
+                        <Grid container spacing={3} sx={{mt: '1%'}}>
+                            <Information header={data.ultimoValorDisponible} title='Último valor disponible' body='TBD databse'/>
+                            <Information header={data.tendenciaActual} title='Tendencia actual' body={data.tendenciaDeseada}/>
+                        </Grid>
+                        <Grid container spacing={3} sx={{mt: '1%'}}>
+                            <Information header={data.anioUltimoValorDisponible} title='Año de referencia' body='Año al que se refiere el último valor disponible'/>
+                            <Information header={data.CoberturaGeografica} title='Cobertura geográfica' body='Territorio al que se refiere la captación de datos'/>
+                        </Grid>
+                </Grid>
+                
+                <Grid item xs={12} md={6} sx={{bgcolor: '', display:`flex`, justifyContent:`center`, alignItems:`center`}}>
+                    <Grid sx={ theme => (
+                      {
+                      borderRadius:`5px`, 
+                      bgcolor: 'cardInformation.main', 
+                      color:`white`,
+                      height:`96%`, 
+                      mt:`5%`,
+                      ml:`5%`, 
+                      textAlign:`center`, 
+                      width:`95%`,
+                      [theme.breakpoints.down('md')]: {
+                        width:`100%`,
+                        ml:`0`, 
+                      },
+                      })}>
+                      <Box sx={{ml:`10%`,mr:`10%`}}>
+                    <br />
+                    <h2>Formula</h2>
+                     <MathJax.Provider>
+                    <h1 className="formulaText"><MathJax.Node inline formula={formulaTest} /></h1>
+                    </MathJax.Provider>
+                    <h3>Descripción</h3>
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <h3>Fuente de consulta</h3>
+                    <p>TEST.INC</p>
+                      </Box>
+                    </Grid>
+                </Grid>
             </Grid>
-            <Grid container spacing={3} sx={{ mt: "1%" }}>
-              <Information
-                header={data.anioUltimoValorDisponible}
-                title="Año de referencia"
-                body="Año al que se refiere el último valor disponible"
-              />
-              <Information
-                header={data.CoberturaGeografica}
-                title="Cobertura geográfica"
-                body="Territorio al que se refiere la captación de datos"
-              />
-            </Grid>
-          </Grid>
-
-          <Grid
-            container
-            xs={12}
-            md={6}
-            sx={{
-              bgcolor: "",
-              display: `flex`,
-              justifyContent: `center`,
-              alignItems: `center`,
-            }}
-          >
-            <Grid
-              sx={{
-                bgcolor: "cardInformation.main",
-                height: `96%`,
-                width: `95%`,
-                mt: `5%`,
-                ml: `5%`,
-                textAlign: `center`,
-                color: `white`,
-                borderRadius: `5px`,
-              }}
-            >
-              <br />
-              <h2>Formula</h2>
-              <MathJax.Provider>
-                <h1 className="formulaText">
-                  <MathJax.Node inline formula={formulaTest} />
-                </h1>
-              </MathJax.Provider>
-              <h3>Descripción</h3>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum.
-              </p>
-              <h3>Fuente de consulta</h3>
-              <p>TEST.INC</p>
-            </Grid>
-          </Grid>
-        </Grid>
-
-        {/*=============================== Mapa ======================================*/}
-        <Grid container spacing={2} sx={{ mt: "5%" }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexWrap: "wrap",
-              minWidth: 300,
-              width: "100%",
-            }}
-          >
-            <ImageButton
-              focusRipple
-              key={mapImage.title}
-              style={{
-                width: mapImage.width,
-              }}
-            >
-              <ImageSrc style={{ backgroundImage: `url(${mapImage.url})` }} />
-              <ImageBackdrop className="MuiImageBackdrop-root" />
-              <Image>
-                <Typography
-                  component="span"
-                  variant="subtitle1"
-                  color="inherit"
-                  sx={{
-                    position: "relative",
-                    p: 4,
-                    pt: 2,
-                    pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
-                    backgroundColor: "black",
-                    opacity: 0.7,
-                    borderRadius: "10%",
-                  }}
-                >
-                  Ver mapa
-                  <ImageMarked className="MuiImageMarked-root" />
-                </Typography>
-              </Image>
-            </ImageButton>
-          </Box>
-        </Grid>
-
-        <Grid container sx={{mt: '2%'}}>
+        {/*=============================== Graph ======================================*/}
+        <Grid container sx={{mt: '5%'}}>
           <Grid item xs={12} md={6}>
             <CustomTable data={data.Historicos} />
           </Grid>
@@ -272,8 +196,50 @@ export default function FichaTecnica(props) {
             <Graph data={data.Historicos} />
           </Grid>
         </Grid>
-      </Container>
-    </>
+        {/*=============================== Mapa ======================================*/}
+        <Grid container item sx={{mt: '5%', justifyContent:`center`, alignItems:`center`}} >
+          <Box sx={ theme => (
+            { 
+            display: 'flex', 
+            flexWrap: 'wrap', 
+            minWidth: 300, 
+            width: '100%',
+
+          })
+            }>
+        <ImageButton
+          focusRipple
+          key={mapImage.title}
+          style={{
+            width: mapImage.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${mapImage.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
+              sx={{
+                position: 'relative',
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                backgroundColor: 'black',
+                opacity: 0.7,
+                borderRadius: '10%',
+              }}
+            >
+              Ver mapa
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+    </Box>
+          </Grid>
+          </Container>
+          </>
   );
 }
 

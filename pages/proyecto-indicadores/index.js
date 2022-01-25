410098dcfd2b9d1e6ba0e643f8618a7bbe6a6208
Jesus Morales <jmartinmoa@gmail.com>
2022-01-25 12:11:41 -0700

    img to Image fixed (maybe)

--- pages/proyecto-indicadores/index.js
+++ pages/proyecto-indicadores/index.js
@@ -1,8 +1,8 @@
-import { Button, Card, CardActionArea, Container, Grid, Link } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Button, Card, CardActionArea, Container, Grid, Link } from '@mui/material';
 import Head from 'next/head';
 import AdbIcon from '@mui/icons-material/Adb';
 import style from '../../styles/indicador.module.css'
+import Image from 'next/image';
 
 
 
@@ -57,14 +57,14 @@ export default function Modulo(props) {
             <hr className={style.hrMin}/>
             <Grid container spacing={1}>
                 <Grid item xs={12} lg={9}>
-                    <div className={style.leftSide}>
+                    <Box className={style.leftSide}>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum mi vel risus ultricies vestibulum. Maecenas consectetur ut arcu id dignissim. Vivamus lorem purus, malesuada eu nisl nec, pharetra varius purus. In facilisis quis sem in ornare. Suspendisse varius magna sapien, et congue ante cursus eget. Praesent mattis, eros eget congue rutrum, massa lorem auctor neque, a molestie metus risus vel leo. Aenean ac odio nisl.</p>
-                    </div>
+                    </Box>
                 </Grid>
                 <Grid item xs={12} lg={3}>
-                    <div className={style.rightSide}>
-                        <img src='/images/implan-logo.webp' alt='implan_logo'  />
-                    </div>
+                    <Box className={style.rightSide}>
+                    <Image src='/images/implan-logo.webp' alt='implan_logo' width={200} height={150} />
+                    </Box>
                 </Grid>
                 
             </Grid>

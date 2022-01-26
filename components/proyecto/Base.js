ffd8871cc62d7e701a06583672035924eeb5c299
Luis Cruz <luisccc99@gmail.com>
2022-01-26 15:01:16 -0700

    change muilink to nextlink to fix page transitions

--- components/proyecto/Base.js
+++ components/proyecto/Base.js
@@ -1,7 +1,8 @@
-import {Avatar, Card, CardContent, CardHeader, CardActions, Typography, Link, Button, ThemeProvider} from '@mui/material';
+import { Avatar, Card, CardContent, CardHeader, CardActions, Typography, Button, ThemeProvider } from '@mui/material';
 import AttractionsIcon from '@mui/icons-material/Attractions';
 import styled from "@mui/system/styled";
 import theme from '../../styles/theme';
+import NextLink from 'next/link';
 
 const SecondaryColorButton = styled(Button)(({ theme }) => ({
     color: 'white',
@@ -11,41 +12,41 @@ const SecondaryColorButton = styled(Button)(({ theme }) => ({
     },
 }));
 
-const Base = ({title, body, url}) => {
+const Base = ({ title, body, url }) => {
     return (
         <>
-            <Card sx={{maxWidth: 900, boxShadow:30}} variant='outlined'>
-                    <CardHeader
-                        avatar={
-                            <Avatar
-                                sx={{bgcolor: 'secondary.main'}}
-                                aria-label={`Icono de proyecto ${title}`}
-                            >
-                                <AttractionsIcon />
-                            </Avatar>
-                        }
-                        title={title}
-                        titleTypographyProps={{variant:'h5'}}
-                    />
+            <Card sx={{ maxWidth: 900, boxShadow: 30 }} variant='outlined'>
+                <CardHeader
+                    avatar={
+                        <Avatar
+                            sx={{ bgcolor: 'secondary.main' }}
+                            aria-label={`Icono de proyecto ${title}`}
+                        >
+                            <AttractionsIcon />
+                        </Avatar>
+                    }
+                    title={title}
+                    titleTypographyProps={{ variant: 'h5' }}
+                />
 
-                    <CardContent>
-                        <Typography variant="body" style={{whiteSpace: 'pre-line'}}>
-                            {body} 
-                        </Typography>
+                <CardContent>
+                    <Typography variant="body" style={{ whiteSpace: 'pre-line' }}>
+                        {body}
+                    </Typography>
 
-                    </CardContent>
-                    <CardActions disableSpacing sx={{display: 'flex', justifyContent:'flex-end'}}>
-                        <Button variant='contained' size='small' sx={{bgcolor: 'primary.main'}}>
-                            <Link href={`/${url}`} style={{textDecoration: 'none'}}>
-                                <ThemeProvider theme={theme}>
-                                    <Typography variant="body" sx={{fontWeight: 'bold', color: 'primary.buttonColor'}}>
-                                        Ver más
-                                    </Typography>
-                                 </ThemeProvider>
-                            </Link>
-                        </Button>
-                    </CardActions>
-                </Card>
+                </CardContent>
+                <CardActions disableSpacing sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    <Button variant='contained' size='small' sx={{ bgcolor: 'primary.main' }}>
+                        <NextLink href={`/${url}`} style={{ textDecoration: 'none' }} passHref>
+                            <a>
+                                <Typography variant="body" sx={{ fontWeight: 'bold', color: 'primary.buttonColor' }}>
+                                    Ver más
+                                </Typography>
+                            </a>
+                        </NextLink>
+                    </Button>
+                </CardActions>
+            </Card>
         </>
     )
 }

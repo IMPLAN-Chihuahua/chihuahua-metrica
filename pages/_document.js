bc27679ec44f6ead3e5e651830acc361c215878d
Jesus Morales <jmartinmoa@gmail.com>
2022-01-27 11:55:49 -0700

    loading screen added

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />

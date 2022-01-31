a3174f6d5d86e73f7fc5dbc69bdda1784cf6dfa1
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-01-31 11:27:17 -0700

    Added validation when historical values are nil

--- components/indicador/Datasheet/GraphBox.js
+++ components/indicador/Datasheet/GraphBox.js
@@ -2,10 +2,19 @@ import React from 'react';
 import Graph from "@components/indicador/Datasheet/Graph";
 import CustomTable from "@components/indicador/Datasheet/CustomTable";
 import Title from "@components/commons/Title";
-import {Grid} from "@mui/material";
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
 
 const GraphBox = (history) => {
-    const {history:data} = history;
+  if(history.Historicos === undefined){
+    return HistoricalValuesNotExists(history);
+  }else {
+    return HistoricalValuesExists(history);
+  }
+};
+
+function HistoricalValuesExists(history){
+  const {history:data} = history;
   return (
    <>
    <Title margin={'3% 0 0 0'} variant={'h4'} content="Valores históricos"></Title>
@@ -18,6 +27,34 @@ const GraphBox = (history) => {
           </Grid>
         </Grid>
   </>
+  );
+};
+
+function HistoricalValuesNotExists(){
+  return (
+        <>
+          <Grid item xs={12} md={12} sx={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <Grid sx={ theme => (
+            {
+            borderRadius:'5px', 
+            bgcolor: 'cardInformation.main', 
+            color:'white',
+            height:'96%', 
+            mt:'5%',
+            textAlign:'center', 
+            width:'95%',
+            [theme.breakpoints.down('md')]: {
+            width:'100%',
+            },
+            })}>
+                <Box>
+                    <br />
+                    <h1 className="formulaText">¡No hay datos históricos disponibles para este indicador!</h1>
+                    <br />
+                </Box>
+            </Grid>
+          </Grid>
+        </>
   );
 };
 

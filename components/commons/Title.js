67fe45279cdf690b879fab1ad5303f2855739a79
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-02-02 12:30:37 -0700

    Changed Title attributes

--- components/commons/Title.js
+++ components/commons/Title.js
@@ -2,12 +2,12 @@ import { Box } from '@mui/system';
 import styles from './Title.module.css';
 const { Typography, Divider } = require("@mui/material");
 
-const Title = ({variant, content, margin, lineStart = 'auto'}) => {
+const Title = ({variant, content, margin, lineStart = 'auto', children, fontWeight = 'regular'}) => {
     return (
         <>
-        <Box sx={{display: 'flex', m: {margin}}} className={styles.titleGrandfather}>
-            <Box m="auto" className={styles.titleFather}>
-                <Typography className={styles.titleContent} variant={variant}>{content}</Typography>
+        <Box sx={{display: 'flex', m: {margin}}}>
+            <Box className={styles.titleFather}>
+                <Typography className={styles.titleContent} variant={variant} sx={{fontWeight: {fontWeight}}}>{children}</Typography>
                 <Divider sx={{m: lineStart}} className={styles.titleDivider}></Divider>
             </Box>
         </Box>  

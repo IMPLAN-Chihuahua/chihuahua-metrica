9d5266f48c5c40201093c140d5769e763350393c
Luis Cruz <luisccc99@gmail.com>
2022-05-30 15:09:50 -0600

    Refactor title component to accept props of typography

--- components/commons/Title.js
+++ components/commons/Title.js
@@ -1,25 +1,14 @@
-import styles from './Title.module.css';
 import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
 import Box from '@mui/material/Box';
-import { toTitleCase } from 'helpers/StringUtils';
 
-const Title = ({ variant, component, margin, lineStart = 'auto', children, fontWeight = 'regular' }) => {
-    component = component !== null ? component : variant;
-    return (
-        <>
-            <Box sx={{ display: 'flex', m: { margin } }}>
-                <Box className={styles.titleFather}>
-                    <Typography
-                        className={styles.titleContent}
-                        variant={variant}
-                        component={component}
-                        sx={{ fontWeight: { fontWeight } }}>{toTitleCase(children)}
-                    </Typography>
-                    <Divider sx={{ m: lineStart }} className={styles.titleDivider}></Divider>
-                </Box>
-            </Box>
-        </>);
+const Title = ({ children, ...props }) => {
+  return (
+    <Box>
+      <Typography
+        {...props}
+        style={{ position: 'relative' }}>{children}</Typography>
+    </Box>
+  );
 };
 
 export default Title;

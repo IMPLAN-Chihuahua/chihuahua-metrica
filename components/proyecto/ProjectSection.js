2ef12fd4f732b25a817fb5889cf41a91e2b5aafb
Luis Cruz <luisccc99@gmail.com>
2022-05-30 09:41:19 -0600

    Add project cards and their images as well. Add the colors of the app in global style. Rename some files to make clearer the code

--- components/proyecto/ProjectSection.js
+++ components/proyecto/ProjectSection.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import GridBase from '@components/proyecto/GridBase';
+import ProjectList from '@components/proyecto/ProjectList';
 import Container from "@mui/material/Container";
 import { Box } from '@mui/material';
 import Title from '@components/commons/Title';
 
 
-export const Proyectos = () => {
+export const ProjectsSection = () => {
   return (
     <>
       <Box sx={{ backgroundColor: 'rgb(38, 48, 68,0.03)', width: '100%' }}>
@@ -13,7 +13,7 @@ export const Proyectos = () => {
           <Box sx={{ display: 'flex', justifyContent: 'center'}}>
             <Title variant='h4' component='h2'>Proyectos</Title>
           </Box>
-          <GridBase />
+          <ProjectList />
         </Container>
       </Box>
     </>

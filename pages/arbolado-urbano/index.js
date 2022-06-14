326e33e7dd934f74543268ef287771bafb4c9529
Luis Cruz <luisccc99@gmail.com>
2022-06-14 10:23:11 -0600

    Change layout of 'nuestra ciudad' section, use Grids

--- pages/arbolado-urbano/index.js
+++ pages/arbolado-urbano/index.js
@@ -1,6 +1,6 @@
 import Title from '@components/commons/Title';
 import { Android, GitHub, OpenInNew } from '@mui/icons-material';
-import { Container, Typography, Box, Button } from '@mui/material';
+import { Container, Typography, Box, Button, Grid } from '@mui/material';
 import Head from 'next/head';
 
 const ArboladoUrbano = () => {
@@ -31,91 +31,112 @@ const ArboladoUrbano = () => {
         </section>
         <section>
           <Title variant='h4' component='h2'>En nuestra ciudad...</Title>
-          <Box display='flex' justifyContent='flex-start' alignItems='center'>
-            <Box sx={{
-              backgroundColor: 'aliceBlue',
-              padding: 3,
-              width: 'fit-content',
-              borderRadius: 5,
-              order: 1,
-            }} mb={2}>
-              <Typography
-                textAlign='center'
-                sx={{
-                  fontSize: '3rem',
-                  fontWeight: 500,
-                  color: 'primary.blue'
-                }}>0.23</Typography>
-              <Typography textAlign='center' fontWeight='bold'>Arboles por persona</Typography>
-            </Box>
-            <Box sx={{
-              height: '300px',
-              backgroundColor: 'lightgray',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: 3,
-              marginRight: 3,
-              flexGrow: 1,
-            }}>
-              <Typography textAlign='center' sx={{ fontWeight: 500 }}>Mapa</Typography>
-            </Box>
-          </Box>
-          <Box display='flex' justifyContent='flex-start' alignItems='center'>
-            <Box sx={{ backgroundColor: 'aliceBlue', padding: 3, width: 'fit-content', borderRadius: 5 }} mb={2}>
-              <Typography
-                textAlign='center'
-                sx={{
-                  fontSize: '3rem',
-                  fontWeight: 500,
-                  color: 'primary.blue'
-                }}>11.3%</Typography>
-              <Typography textAlign='center' fontWeight='bold'>Areas verdes por persona</Typography>
-            </Box>
-            <Box sx={{
-              height: '300px',
-              backgroundColor: 'lightgray',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: 3,
-              flexGrow: 1,
-              marginLeft: 3
-            }}>
-              <Typography textAlign='center' sx={{ fontWeight: 500 }}>Mapa</Typography>
-            </Box>
-          </Box>
-          <Box display='flex' justifyContent='flex-start' alignItems='center'>
-            <Box sx={{
-              backgroundColor: 'aliceBlue',
-              padding: 3,
-              width: 'fit-content',
-              borderRadius: 5,
-              order: 1
-            }} mb={2}>
-              <Typography
-                textAlign='center'
-                sx={{
-                  fontSize: '3rem',
-                  fontWeight: 500,
-                  color: 'primary.blue',
-                }}>60%</Typography>
-              <Typography textAlign='center' fontWeight='bold'>De la población</Typography>
-              <Typography textAlign='center' fontWeight='bold'>vive a 300 metros de un parque</Typography>
-            </Box>
-            <Box sx={{
-              height: '300px',
-              backgroundColor: 'lightgray',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-              marginBottom: 3,
-              flexGrow: 1,
-              marginRight: 3,
-            }}>
-              <Typography textAlign='center' sx={{ fontWeight: 500 }}>Mapa</Typography>
-            </Box>
-          </Box>
+          <section>
+            <Grid container>
+              <Grid item container mb={{ xs: 2, md: 0 }}>
+                <Grid item xs={12} lg={8}>
+                  <Box sx={{
+                    backgroundColor: 'lightslategray',
+                    margin: 1,
+                    height: '300px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                    Mapa
+                  </Box>
+                </Grid>
+                <Grid
+                  item
+                  xs={12}
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  lg={4}
+                  sx={{ order: { xs: -1, lg: 1 } }}>
+                  <Box
+                    sx={{
+                      backgroundColor: '#ecf8f8',
+                      padding: 5,
+                      borderRadius: 5,
+                      color: '#22577a'
+                    }}
+                  >
+                    <Typography fontWeight='bold' textAlign='center' fontSize={30}>0.23</Typography>
+                    <Typography fontWeight='500'>Arboles</Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Grid item container mb={{ xs: 2, md: 0 }}>
+                <Grid
+                  item
+                  xs={12}
+                  lg={4}
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'>
+                  <Box
+                    sx={{
+                      backgroundColor: '#ecf8f8',
+                      padding: 5,
+                      borderRadius: 5,
+                      color: '#22577a'
+                    }}
+                  >
+                    <Typography fontWeight='bold' textAlign='center' fontSize={30}>30</Typography>
+                    <Typography fontWeight='500'>Areás verdes por persona</Typography>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} lg={8}>
+                  <Box sx={{
+                    backgroundColor: 'lightslategray',
+                    margin: 1,
+                    height: '300px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}>
+                    Mapa
+                  </Box>
+                </Grid>
+              </Grid>
+              <Grid item container mb={{ xs: 2, md: 0 }}>
+                <Grid item xs={12} lg={8}>
+                  <Box sx={{
+                    backgroundColor: 'lightslategray',
+                    margin: 1,
+                    height: '300px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}>
+                    Mapa
+                  </Box>
+                </Grid>
+                <Grid
+                  item
+                  display='flex'
+                  justifyContent='center'
+                  alignItems='center'
+                  xs={12}
+                  lg={4}
+                  sx={{ order: { xs: -1, lg: 1 } }}>
+                  <Box
+                    sx={{
+                      backgroundColor: '#ecf8f8',
+                      padding: 5,
+                      borderRadius: 5,
+                      color: '#22577a'
+                    }}
+                  >
+                    <Typography fontWeight='bold' textAlign='center' fontSize={30}>60%</Typography>
+                    <Typography fontWeight='500' textAlign='center'>De la población vive</Typography>
+                    <Typography fontWeight='500' textAlign='center'>cerca de un parque</Typography>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Grid>
+          </section>
           <Title variant='h5' component='h3'>Fuente</Title>
           <Typography mb={1}>Et quod magni cumque. Eaque fugit ducimus mollitia est. Debitis cumque impedit magnam molestias voluptatem. Consequatur commodi doloribus aspernatur dolor.</Typography>
           <Button

0fb0b64088b51c30488d0a8eda0a3d6587911784
Miguel Valdez <mike.281299@gmail.com>
2022-07-05 10:07:37 -0600

    Fixed 404 page responsiveness

---
+++ components/commons/PartnersStrip.js
@@ -0,0 +1,23 @@
+import { Container, Grid } from '@mui/material'
+import Image from 'next/image'
+import React from 'react'
+
+import style from './PartnersStrip.module.css'
+
+const PartnersStrip = () => {
+    const partnerList = ['CPUM', 'IMPLAN', 'SIGMUN']
+
+    return (
+        <Container className={`${style.partnerContainer}`}>
+            <Grid container>
+                {partnerList.map((partner, index) => (
+                    <Grid item xs={6} sm={6} md={4} key={index} className={`${style.partnerImagesContainer}`}>
+                        <Image src={`/images/partners/${partner}.png`} alt={partner} height='100' width='100' objectFit='contain' title={`Logotipo de ${partner}`} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
+    )
+}
+
+export default PartnersStrip

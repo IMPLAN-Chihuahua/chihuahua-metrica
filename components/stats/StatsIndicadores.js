966f2689f41fecb92aa991cc3a83c476416cf996
Luis Cruz <luisccc99@gmail.com>
2022-07-19 10:44:23 -0600

    Fix bug in indicadores stats

--- components/stats/StatsIndicadores.js
+++ components/stats/StatsIndicadores.js
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import Image from 'next/image'
 import React, { useState } from 'react'
 import style from './StatsIndicadores.module.css'
@@ -8,22 +8,20 @@ const Stat = ({ indicador, indextest }) => {
 
   return (
     <Grid item xs={12} md={3}>
-      <div className={style.statsContainer}>
-        <div
-          className={`${style.circle}`}
-          style={{ backgroundImage: isHovering ? `url('/images/stats/rounded-images/${indextest + 1}.png')` : undefined }}
-          onMouseEnter={() => setIsHovering(true)}
-          onMouseLeave={() => setIsHovering(false)}
-        >
-          <div className={isHovering ? style.overlay : undefined}>
-            <Box className={`${style.circleStats}`} style={{ filter: isHovering ? 'invert(100%)' : undefined }}>
-              <Image src={indicador.icon} width={100} height={100} />
-            </Box>
-            <Typography textAlign='center' className={`${style.circleStatsDescription}`}>{indicador.value}</Typography>
-          </div>
+      <div
+        className={`${style.circle}`}
+        style={{ backgroundImage: isHovering ? `url('/images/stats/rounded-images/${indextest + 1}.png')` : undefined }}
+        onMouseEnter={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        <div className={isHovering ? style.overlay : undefined}>
+          <Box className={`${style.circleStats}`} style={{ filter: isHovering ? 'invert(100%)' : undefined }}>
+            <Image src={indicador.icon} width={100} height={100} />
+          </Box>
+          <Typography textAlign='center' className={`${style.circleStatsDescription}`}>{indicador.value}</Typography>
         </div>
-        <p className={style.indicador}>{indicador.name}</p>
       </div>
+      <p className={style.indicador}>{indicador.name}</p>
     </Grid>
   );
 }
@@ -49,7 +47,7 @@ const StatsIndicadores = () => {
   }];
 
   return (<>
-    {<Grid container className={style.test}>
+    {<Grid container sx={{ alignItems: 'stretch', backgroundColor: 'primary.main', mt: 3, mb: 2 }}>
       {somearray.map((item, index) => (
         <Stat key={index} indicador={item} indextest={index} />
       ))}

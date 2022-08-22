e980ffc17db8b1c781d07fe79cf1557e5cd4e412
Miguel Valdez <mike.281299@gmail.com>
2022-08-22 11:26:46 -0600

    Disabled tree datasheet when hovering

--- components/arbolado/TextWithAvatar.js
+++ components/arbolado/TextWithAvatar.js
@@ -7,7 +7,7 @@ import style from './TextWithAvatar.module.css'
 
 const TextWithAvatar = ({ children, flexDirection, avatar, subtext, header }) => {
     return (
-        <Box className={`${style.bodyContainer} ${header ? style.header : undefined}`} sx={{ flexDirection: flexDirection }}>
+        <Box className={`${style.bodyContainer} ${header ? style.header : undefined}`} sx={{ flexDirection: flexDirection, mb: 1 }}>
             <Avatar src={avatar} className={`${style.imageContainer}`} sx={{ width: 100, height: 100 }} />
             <Box className={`${style.bodyText}`}>
                 <Title variant='h5' component='h5' color='var(--darker-green)'>{children}</Title>

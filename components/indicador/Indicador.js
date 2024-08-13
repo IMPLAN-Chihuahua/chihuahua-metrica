966782795fb80fec1d8e9647992d8099b35b580d
Luis Cruz <luisccc99@gmail.com>
2024-08-13 10:08:35 -0600

    Display chip to show if indicador is destacado based on an objetivo

--- components/indicador/Indicador.js
+++ components/indicador/Indicador.js
@@ -5,6 +5,7 @@ import Grid from '@mui/material/Grid';
 import CardActionArea from '@mui/material/CardActionArea';
 import NextLink from 'next/link';
 import { numberWithCommas } from 'helpers/FormatNumbers';
+import { Chip, Tooltip } from '@mui/material';
 
 export const ODS = 1;
 export const UNIDAD_MEDIDA = 2;
@@ -14,93 +15,134 @@ const compareIds = (a, b) => a.idCatalogo - b.idCatalogo;
 
 const Indicador = (props) => {
   const indicador = props.value;
+  const objetivo = indicador.objetivos[0]
   return (
-    <Card variant='outlined'>
-      <CardActionArea sx={{ padding: 2 }}>
-        <NextLink href={`/chihuahua-en-datos/indicadores/${indicador.id}`} passHref>
-          <a>
-            <Grid container justifyContent='center' rowSpacing={2} columnSpacing={2}>
-              <Grid item sm container direction='column'>
-                <Grid item>
-                  <Typography
-                    variant='h6'
-                    component='h3'
-                    fontWeight='bold'
-                    mb={2}
-                    textAlign={{ xs: 'center', md: 'left' }}
-                  >
-                    {indicador.nombre}
-                  </Typography>
-                </Grid>
-                <Grid
-                  item
-                  xs
-                  container
-                  direction='row'
-                  justifyContent='space-evenly'
-                  rowSpacing={2}
-                >
-                  <Grid
-                    item
-                    xs={4}
-                    md={2}
-                    textAlign='center'
-                    container
-                    display='flex'
-                    flexDirection='column'
-                    justifyContent='space-between'
-                  >
-                    <Typography
-                      fontWeight='bold'
-                    >
-                      Último Valor Disponible
-                    </Typography>
-                    <Typography
-                      color={props.fontColor}
-                      fontWeight='bold'
-                      fontSize={25}
-                    >{numberWithCommas(indicador.ultimoValorDisponible)}
-                    </Typography>
-                  </Grid>
-                  <Divider
-                    orientation='vertical'
-                    variant='middle'
-                    flexItem
-                    sx={{ borderRightWidth: 3 }}
-                  />
-                  <Grid
-                    item
-                    xs={4}
-                    md={2}
-                    container
-                    textAlign='center'
-                    display='flex'
-                    flexDirection='column'
-                    justifyContent='space-between'
-                  >
-                    <Typography
-                      fontWeight='bold'
-                    >
-                      Año de Referencia
-                    </Typography>
-                    <Typography
-                    >
-                      {indicador.anioUltimoValorDisponible}
-                    </Typography>
-                  </Grid>
-                  <Divider
-                    orientation='vertical'
-                    variant='middle'
-                    flexItem
-                    sx={{ borderRightWidth: 3, display: { xs: 'none', md: 'block' } }}
-                  />
+    <Tooltip title={objetivo?.destacado ? `Este indicador abona principalmente para lograr el objetivo '${objetivo.titulo}'` : ``} placement='left-start'>
+      <Card variant='outlined'>
+        <CardActionArea sx={{ p: 2 }}>
+          <NextLink href={`/chihuahua-en-datos/indicadores/${indicador.id}`} passHref>
+            <a>
+              {objetivo?.destacado && (
+                <Chip
+                  sx={{ backgroundColor: '#08203E', color: 'white', }}
+                  label='destacado'
+                  size='small'
+                />
 
-                  {
-                    indicador.catalogos.sort(compareIds).map((catalogo, index) => {
-                      if (catalogo.idCatalogo !== ODS) {
-                        if (catalogo.idCatalogo === UNIDAD_MEDIDA) {
-                          return (
-                            <>
+              )}
+              <Grid container justifyContent='center' rowSpacing={2} columnSpacing={2}>
+                <Grid item sm container direction='column'>
+                  <Grid item>
+                    <Typography
+                      variant='h6'
+                      component='h3'
+                      fontWeight='bold'
+                      mb={2}
+                      textAlign={{ xs: 'center', md: 'left' }}
+                    >
+                      {indicador.nombre}
+                    </Typography>
+                  </Grid>
+                  <Grid
+                    item
+                    xs
+                    container
+                    direction='row'
+                    justifyContent='space-evenly'
+                    rowSpacing={2}
+                  >
+                    <Grid
+                      item
+                      xs={4}
+                      md={2}
+                      textAlign='center'
+                      container
+                      display='flex'
+                      flexDirection='column'
+                      justifyContent='space-between'
+                    >
+                      <Typography
+                        fontWeight='bold'
+                      >
+                        Último Valor Disponible
+                      </Typography>
+                      <Typography
+                        color={props.fontColor}
+                        fontWeight='bold'
+                        fontSize={25}
+                      >{numberWithCommas(indicador.ultimoValorDisponible)}
+                      </Typography>
+                    </Grid>
+                    <Divider
+                      orientation='vertical'
+                      variant='middle'
+                      flexItem
+                      sx={{ borderRightWidth: 3 }}
+                    />
+                    <Grid
+                      item
+                      xs={4}
+                      md={2}
+                      container
+                      textAlign='center'
+                      display='flex'
+                      flexDirection='column'
+                      justifyContent='space-between'
+                    >
+                      <Typography
+                        fontWeight='bold'
+                      >
+                        Año de Referencia
+                      </Typography>
+                      <Typography
+                      >
+                        {indicador.anioUltimoValorDisponible}
+                      </Typography>
+                    </Grid>
+                    <Divider
+                      orientation='vertical'
+                      variant='middle'
+                      flexItem
+                      sx={{ borderRightWidth: 3, display: { xs: 'none', md: 'block' } }}
+                    />
+
+                    {
+                      indicador.catalogos.sort(compareIds).map((catalogo, index) => {
+                        if (catalogo.idCatalogo !== ODS) {
+                          if (catalogo.idCatalogo === UNIDAD_MEDIDA) {
+                            return (
+                              <>
+                                <Grid
+                                  item
+                                  xs={4}
+                                  md={2}
+                                  container
+                                  textAlign='center'
+                                  display='flex'
+                                  flexDirection='column'
+                                  justifyContent='space-between'
+                                  key={index}
+                                >
+                                  <Typography
+                                    fontWeight='bold'
+                                  >
+                                    Unidad de Medida
+                                  </Typography>
+                                  <Typography
+                                  >
+                                    {catalogo.nombre || 'NA'}
+                                  </Typography>
+                                </Grid>
+                                <Divider
+                                  orientation='vertical'
+                                  variant='middle'
+                                  flexItem
+                                  sx={{ borderRightWidth: 3 }}
+                                />
+                              </>
+                            )
+                          } else {
+                            return (
                               <Grid
                                 item
                                 xs={4}
@@ -113,58 +155,28 @@ const Indicador = (props) => {
                                 key={index}
                               >
                                 <Typography
-                                  fontWeight='bold'
+                                  fontWeight={'bold'}
                                 >
-                                  Unidad de Medida
+                                  Cobertura Geográfica
                                 </Typography>
                                 <Typography
                                 >
                                   {catalogo.nombre || 'NA'}
                                 </Typography>
                               </Grid>
-                              <Divider
-                                orientation='vertical'
-                                variant='middle'
-                                flexItem
-                                sx={{ borderRightWidth: 3 }}
-                              />
-                            </>
-                          )
-                        } else {
-                          return (
-                            <Grid
-                              item
-                              xs={4}
-                              md={2}
-                              container
-                              textAlign='center'
-                              display='flex'
-                              flexDirection='column'
-                              justifyContent='space-between'
-                              key={index}
-                            >
-                              <Typography
-                                fontWeight={'bold'}
-                              >
-                                Cobertura Geográfica
-                              </Typography>
-                              <Typography
-                              >
-                                {catalogo.nombre || 'NA'}
-                              </Typography>
-                            </Grid>
-                          )
+                            )
+                          }
                         }
-                      }
-                    })
-                  }
+                      })
+                    }
+                  </Grid>
                 </Grid>
               </Grid>
-            </Grid>
-          </a>
-        </NextLink>
-      </CardActionArea>
-    </Card>
+            </a>
+          </NextLink>
+        </CardActionArea>
+      </Card>
+    </Tooltip>
   );
 };
 

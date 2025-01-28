c6f67c9c393f9ef67c57e371564510f3976a36a0
Luis Cruz <luisccc99@gmail.com>
2025-01-28 12:23:00 -0600

    Remove breadcrumbs in contacto page

--- pages/contacto.js
+++ pages/contacto.js
@@ -1,26 +1,16 @@
 import { AboutLocation } from "@components/information/AboutLocation";
 import { Container } from "@mui/material";
-import { AboutForm } from '@components/information/AboutForm';
 import Splashscreen from "@components/commons/Splashscreen";
 import Head from "next/head";
-import PageBreadcrumb from "@components/commons/PageBreadcrumb";
-
-const CRUMBS = [
-  {
-    text: 'Contacto'
-  }
-]
 
 const Contact = () => {
   return (
     <>
       <Head>
         <title>Contacto</title>
-        <link rel="icon" href="/icon.ico" />
       </Head>
       <Splashscreen />
       <Container sx={{ mt: 3 }}>
-        <PageBreadcrumb crumbs={CRUMBS} />
         <AboutLocation />
       </Container>
     </>

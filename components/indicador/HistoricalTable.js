bd24be2b14b1127a35d6beb24073cf8870cdc2a1
Miguel Valdez <mike.281299@gmail.com>
2026-03-17 11:27:39 -0600

    Agregar paginación en tabla de históricos

--- components/indicador/HistoricalTable.js
+++ components/indicador/HistoricalTable.js
@@ -7,15 +7,38 @@ import {
   Table,
   TableCell,
   TableBody,
+  TablePagination
 } from "@mui/material";
+import { useState } from "react";
 
 const HistoricalTable = ({ data, lastSource, lastValue, lastYear }) => {
-  const hasLastYear = data.some((item) => item.anio === lastYear);
+  const hasLastYear = data.some((item) => Number(item.anio) === Number(lastYear));
 
   const combinedData = hasLastYear
     ? [...data]
     : [...data, { anio: lastYear, valor: lastValue, fuente: lastSource }];
-  const sortedData = [...combinedData].sort((a, b) => a.anio - b.anio);
+
+  // Ordenamos y filtramos duplicados
+  const sortedData = combinedData
+    .filter((obj, index, self) => index === self.findIndex((el) => Number(el.anio) === Number(obj.anio)))
+    .sort((a, b) => a.anio - b.anio);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const paginatedData = sortedData.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
 
   return (
     <TableContainer>
@@ -28,8 +51,8 @@ const HistoricalTable = ({ data, lastSource, lastValue, lastYear }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {sortedData.map((historico) => (
-            <TableRow hover key={`table-${historico.anio}`}>
+          {paginatedData.map((historico, index) => (
+            <TableRow hover key={`table-row-${page}-${historico.anio || index}`}>
               <TableCell scope="row" align="right">{historico.anio}</TableCell>
               <TableCell scope="row" align="right">{numberWithCommas(historico.valor)}</TableCell>
               <TableCell scope="row" align="left">{historico.fuente}</TableCell>
@@ -37,6 +60,21 @@ const HistoricalTable = ({ data, lastSource, lastValue, lastYear }) => {
           ))}
         </TableBody>
       </Table>
+
+      {sortedData.length > 5 && (
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={sortedData.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+          labelRowsPerPage="Filas por página:"
+          labelDisplayedRows={({ from, to, count }) => `${from} - ${to} de ${count}`}
+        />
+      )}
+
       <Typography
         variant='caption'
         mt={1}

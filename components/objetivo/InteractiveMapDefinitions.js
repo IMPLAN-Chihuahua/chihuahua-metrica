0661613793ff967a50be9a826664ccff4c6485c9
Miguel Valdez <mike.281299@gmail.com>
2026-03-18 10:27:56 -0600

    Fix warning and change icon.ico

--- components/objetivo/InteractiveMapDefinitions.js
+++ components/objetivo/InteractiveMapDefinitions.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const InteractiveMapDefinitions = () => {
     return (
-        <Box item md={3} lg={3}>
+        <Box >
             <Typography variant='caption'><b>PSMAMS</b> - Programa Sectorial Metropolitano de Accesibilidad y Movilidad Sostenible de Chihuahua.</Typography> <br />
             <Typography variant='caption'><b>PMOTDU</b> - Plan Municipal de Ordenamiento Territorial y Desarrollo Urbano.
             </Typography><br />
